819ea466a20aa4c91ed9044460cca2c2b63c0b7b
jwong1MHS <jasonw5172001@gmail.com>
2021-11-08 21:58:50 -0500

    Removed redundant folders

--- fweefwop/Scripts/baby_rsa_0.py
+++
@@ -1,7 +0,0 @@
-key = "fwopCTF{I_am_learning_RSA_so_exciting}"
-flag = 0
-
-for i in range(len(key)):
-    flag = flag*({True: 1, False: 256}[i==0]) + ord(key[i])
-
-print(flag)
--- fweefwop/Scripts/baby_rsa_1.py
+++
@@ -1,6 +0,0 @@
-import math
-
-a = 643192922209641529679217093842090563198908934967463878287741
-
-print(bytearray.fromhex(hex(a)[2:]).decode())
-print(a.to_bytes(math.ceil(math.log2(a)/8), 'big'))
